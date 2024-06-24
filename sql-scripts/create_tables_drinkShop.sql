08d8281f903a91c34e41c169c2ac3ddfd67f36bd
Grigoris Liaskos <grigorisliaskos911@gmail.com>
2024-06-24 21:53:26 +0300

    Update create_tables_drinkShop.sql

--- sql-scripts/create_tables_drinkShop.sql
+++ sql-scripts/create_tables_drinkShop.sql
@@ -260,7 +260,7 @@ CREATE TABLE `customer` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
---ALTER TABLE `customer` ADD UNIQUE (`email`);
+-- ALTER TABLE `customer` ADD UNIQUE (`email`);
 
 CREATE TABLE `orders` (
   `id` int NOT NULL AUTO_INCREMENT,
